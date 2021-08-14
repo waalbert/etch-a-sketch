6a3ad0473cb14bbf4fa85c0ae3202a9b61612b62
Albert Wang <aw1002345@gmail.com>
2021-08-14 14:42:36 -0700

    added a clear button, but need to fix it

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ for (let i = 0; i < 16; i++) {
     container.appendChild(newDiv);
 }
 
+
+
+const clearBtn = document.querySelector("#clear");
+clearBtn.addEventListener("click", () => {
+    container.textContent = "";
+    const numSquares = prompt("How many squares?");
+    for (let i = 0; i < numSquares; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.id = "one";
+        container.appendChild(newDiv);
+    }
+});
+
 const divs = document.querySelectorAll("#one");
 divs.forEach(div => div.addEventListener("mouseover", () => {
     div.style.backgroundColor = "#444444";
 }));
+
