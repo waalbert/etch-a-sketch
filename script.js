63480117ddd08c80d7407cfa52e5fa6568e9456c
Albert Wang <aw1002345@gmail.com>
2021-08-15 21:00:32 -0700

    fixed the buttons. Etch-a-sketch can now draw using the mouse

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 const container = document.querySelector("#container");
-for (let i = 0; i < 16; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.id = "one";
-    container.appendChild(newDiv);
-}
-
-
-
 const clearBtn = document.querySelector("#clear");
-clearBtn.addEventListener("click", () => {
-    container.textContent = "";
-    const numSquares = prompt("How many squares?");
-    for (let i = 0; i < numSquares; i++) {
-        let newDiv = document.createElement("div");
-        newDiv.id = "one";
-        container.appendChild(newDiv);
-    }
+const alertText = document.querySelector("#text");
+const squaresBtn = document.querySelector("#squaresBtn");
+squaresBtn.addEventListener("click", () => {
+    resetGrid();
+    generateGrid(getNumSquares());
+    addFunctionality();
 });
 
-const divs = document.querySelectorAll("#one");
-divs.forEach(div => div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "#444444";
-}));
+function resetGrid() {
+    container.textContent = "";
+    alertText.textContent = "";
+}
 
+function getNumSquares() {
+    let numSquares = Number(prompt("How many squares?"));
+    if (numSquares === 0) { /* An empty string converted to a number is 0. 
+                            Therefore,if the user cancels the prompt, a 16 x 16 grid will be automatically formed. */
+        numSquares = 16;
+        alertText.textContent = "You canceled the prompt. A 16 x 16 grid has been created for you.";
+    } else if (numSquares > 100) {
+        numSquares = 100;
+        alertText.textContent = "You cannot input a number greater than 100. Instead, a 100 x 100 grid has been created for you.";
+    }
+    container.style["grid-template-rows"] = `repeat(${numSquares}, 1fr)`;
+    container.style["grid-template-columns"] = `repeat(${numSquares}, 1fr)`;
+    return numSquares;
+}
+
+function generateGrid(numSquares) {
+    for (let i = 0; i < numSquares ** 2; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.id = "square";
+        container.appendChild(newDiv);
+    }
+}
+
+function addFunctionality() {
+    const divs = document.querySelectorAll("#square");
+    divs.forEach(div => div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = randomizeColors();
+    }));
+    clearBtn.addEventListener("click", () => divs.forEach(div => div.style.backgroundColor = "#FFFFFF"));
+}
+
+function randomizeColors() {
+    return `rgb(${Math.floor(Math.random() * 257)},${Math.floor(Math.random() * 257)},${Math.floor(Math.random() * 257)})`
+}
